ffb04aa9f05f6a17f54e1f99ca560a5258cd7535
tsheringphuntsho18 <0223031.cst@rub.edu.bt>
2023-08-28 17:38:46 +0600

    first commit

---
+++ code1.py
@@ -0,0 +1,10 @@
+print('Hi')
+print('How are you?')
+print('I am not goood')
+#variable
+a=808
+print(a)
+name='Tshering Phuntsho'
+if name=='Tshering Phuntsho':
+  print('you are onwer of this laptop')
+#python is case sensitive
---
+++ guessingGame.py
@@ -0,0 +1,10 @@
+print('Welcome to Guessing Game')
+name=input('Enter your name: ')
+print('welcome',name)
+num=5
+lnum=input('guess the lucky number: ')
+if lnum==num:
+  print('Congratulation.It is correct')
+else:
+  print('incorrect')
+print('game over')
